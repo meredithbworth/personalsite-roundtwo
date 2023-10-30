e43d41089960300a7272f5ad14f1d5fff44eee60
Meredith Butterworth <meredithbutterworth@gmail.com>
2023-10-30 19:39:41 -0400

    alert/JS function added and linked to home page

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/* A "Hello world" tier Javascript function, loaded via an external file
+e.g., a console.log of a given string */
+
+let string1="Hello World!"
+
+//console.log(string1)
+
+alert(string1)
+
